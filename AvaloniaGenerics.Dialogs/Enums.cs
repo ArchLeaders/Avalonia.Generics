a655e6ea531df3993f06babc6a3406a0d8730ea4
ArchLeaders <archparliament@gmail.com>
2022-10-30 17:47:37 -0700

    Create library initializer; add generic enums

---
+++ AvaloniaGenerics.Dialogs/Enums.cs
@@ -0,0 +1,7 @@
+﻿namespace AvaloniaGenerics.Dialogs
+{
+    public enum MessageBoxButtons { Ok, OkCancel, YesNo, YesNoCancel }
+    public enum MessageBoxResult { Cancel, No, Ok, Yes }
+    public enum Formatting { None, Markdown }
+    public enum BrowserDialog { File, Folder, SaveFile }
+}
---
+++ AvaloniaGenerics.Dialogs/Initializer.cs
@@ -0,0 +1,24 @@
+﻿global using static AvaloniaGenerics.Dialogs.Initializer;
+
+using Avalonia.Controls;
+using Avalonia.VisualTree;
+
+namespace AvaloniaGenerics.Dialogs
+{
+    public static class Initializer
+    {
+        internal static Window? View { get; set; }
+
+        internal static TopLevel? GetTopLevel()
+        {
+            if (View != null) {
+                return View.GetVisualRoot() as TopLevel;
+            }
+            else {
+                throw new Exception($"Could not find visual root in library '{nameof(Dialogs)}'. Make sure {nameof(Dialogs)} is intialized with 'IVisual.InitializeGenericDialogs()'.");
+            }
+        }
+
+        public static void InitializeGenericDialogs(Window view) => View = view;
+    }
+}
