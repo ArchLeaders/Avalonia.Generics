ed19950837eb03678761d9680bec86b5b81fc65b
ArchLeaders <archparliament@gmail.com>
2022-11-21 13:28:41 -0800

    Task synchronous extensions

---
+++ Avalonia.Generics/Extensions/TaskExtension.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Threading;
+
+namespace Avalonia.Generics.Extensions
+{
+    public static class TaskExtension
+    {
+        public static void RunSync(this Task task)
+        {
+            using var source = new CancellationTokenSource();
+            task.ContinueWith(t => source.Cancel(), TaskScheduler.FromCurrentSynchronizationContext());
+            Dispatcher.UIThread.MainLoop(source.Token);
+        }
+    }
+}
