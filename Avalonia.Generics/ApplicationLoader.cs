055f235b80a7fdd79cee0051f38b70975c490920
ArchLeaders <archparliament@gmail.com>
2022-11-21 13:27:46 -0800

    Refactor application loader

--- Avalonia.Generics/ApplicationLoader.cs
+++ Avalonia.Generics/ApplicationLoader.cs
@@ -1,9 +1,6 @@
-﻿using Avalonia;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Media;
 using Avalonia.Media.Imaging;
-using Avalonia.Platform;
 using Avalonia.VisualTree;
 
 namespace Avalonia.Generics
@@ -16,23 +13,26 @@ namespace Avalonia.Generics
         public static void Attach(Application app)
         {
             if (app.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop && desktop.MainWindow != null) {
-                App.DefaultIcon = new Bitmap(AvaloniaLocator.Current.GetService<IAssetLoader>()!.Open(new($"avares://Avalonia.Generics/Assets/avalonia.ico")));
                 App.View = desktop.MainWindow;
-
-                // Load app icon
-                if (App.View.Icon != null) {
-                    using MemoryStream stream = new();
-                    App.View.Icon.Save(stream);
-                    stream.Position = 0;
-                    App.Icon = new Bitmap(stream);
-                }
-
-                // Construct TopLevel
-                App.TopLevel = App.View.GetVisualRoot() as TopLevel ?? throw new Exception($"Could not find visual root on '{App.View.GetType().FullName}'.");
+                App.Icon = GetWindowIcon(App.View.Icon);
+                App.TopLevel = App.View.GetVisualRoot() as TopLevel
+                    ?? throw new Exception($"Could not find visual root on '{App.View.GetType().FullName}'.");
             }
             else {
                 throw new NotImplementedException();
             }
+        }
+
+        internal static Bitmap? GetWindowIcon(WindowIcon? icon)
+        {
+            if (icon != null) {
+                using MemoryStream stream = new();
+                icon.Save(stream);
+                stream.Position = 0;
+                return new Bitmap(stream);
+            }
+
+            return null;
         }
     }
 }
