5bc85bc8bb8bf110319a056a84314a57c6f7fb76
ArchLeaders <archparliament@gmail.com>
2022-11-21 13:14:49 -0800

    Simplify GenericWindow constructor (use WindowBuilder instead)

--- Avalonia.Generics/Controls/GenericWindow.axaml.cs
+++ Avalonia.Generics/Controls/GenericWindow.axaml.cs
@@ -15,51 +15,8 @@ namespace Avalonia.Generics.Controls
         public DialogResult Result { get; set; }
 
         public GenericWindow() => InitializeComponent();
-        internal GenericWindow(
-            string? title, object? content, object? menu, object? chrome, bool canResize = false, bool canMinimize = true, double minWidth = 0, double minHeight = 0, double maxWidth = double.NaN, double maxHeight = double.NaN, IImage? icon = null)
-        {
-            InitializeComponent();
 
-            MinWidth = minWidth;
-            MinHeight = minHeight;
-            MaxWidth = maxWidth;
-            MaxHeight = maxHeight;
-
-            // Set title
-            Title = title;
-            TitleBox.Text = title;
-
-            // Set resize modes
-            CanResize = canResize;
-            Fullscreen.IsVisible = canResize;
-            Minimize.IsVisible = canMinimize;
-
-            // Initialize chrome events
-            Minimize.Click += (s, e) => WindowState = WindowState.Minimized;
-            Fullscreen.Click += (s, e) => WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
-            Quit.Click += (s, e) => {
-                Result = DialogResult.Cancel;
-                Close();
-            };
-
-            // Load icon
-            DialogIcon.Source = icon ?? App.Icon ?? App.DefaultIcon;
-            Icon = new((IBitmap)DialogIcon.Source);
-
-            // Load Menu
-            if (menu != null) {
-                TitleBox.IsVisible = false;
-                RootMenu.Items = MenuFactory.MenuFactory.Generate(menu);
-            }
-
-            // Load content
-            Content.Content = content;
-
-            Loaded += (s, e) => {
-                MinWidth = MinWidth == 0 ? Bounds.Width : MinWidth;
-                MinHeight = MinHeight == 0 ? Bounds.Height : MinHeight;
-            };
-        }
+        public async Task ShowDialog() => await ShowDialog(App.View);
 
         protected override void HandleWindowStateChanged(WindowState state)
         {
