3470064624e463eb45d7628ad2850d060917f270
ArchLeaders <archparliament@gmail.com>
2022-11-21 13:26:58 -0800

    Use WindowBuilder in MessageBox

--- Avalonia.Generics/Dialogs/MessageBox.axaml.cs
+++ Avalonia.Generics/Dialogs/MessageBox.axaml.cs
@@ -1,115 +1,55 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Layout;
-using Avalonia.Markup.Xaml;
-using Avalonia.Threading;
-using Markdown.Avalonia;
+using Avalonia.Generics.Builders;
+using Avalonia.Generics.Controls;
+using Avalonia.Generics.Factories;
 using Material.Icons;
 
 namespace Avalonia.Generics.Dialogs
 {
     public partial class MessageBox : UserControl
-
-    public partial class MessageBox : Window
     {
-        public MessageBox() => AvaloniaXamlLoader.Load(this);
-        public MessageBox(string title, string text, Formatting formatting, MaterialIconKind? icon = null)
+        private Formatting Formatting { get; } = Formatting.None;
+
+        [Chrome(MaterialIconKind.ContentCopy)]
+        public async void Copy(Window window)
         {
-            AvaloniaXamlLoader.Load(this);
+            string text = Formatting == Formatting.Markdown ? $"\n{MarkdownViewer.Markdown}" : $"```\n{TextViewer.Text}\n```";
+            await Application.Current!.Clipboard!.SetTextAsync($"**{window.Title}**\n{text}");
+        }
 
-            if (icon != null) {
-                this.FindControl<Image>("DefaultIco")!.IsVisible = false;
-                Material.Icons.Avalonia.MaterialIcon materialIco = this.FindControl<Material.Icons.Avalonia.MaterialIcon>("MaterialIco")!;
-                materialIco.IsVisible = true;
-                materialIco.Kind = (MaterialIconKind)icon;
-            }
+        public MessageBox() { }
+        public MessageBox(string text, Formatting formatting = Formatting.None)
+        {
+            InitializeComponent();
+            Formatting = formatting;
 
-            var tb = this.FindControl<TextBox>("Text")!;
             if (formatting == Formatting.Markdown) {
-                tb.IsVisible = false;
-
-                var mdViewer = this.FindControl<MarkdownScrollViewer>("Markdown")!;
-                mdViewer.IsVisible = true;
-                mdViewer.Markdown = text;
+                TextViewer.IsVisible = false;
+                MarkdownViewer.IsVisible = true;
+                MarkdownViewer.Markdown = text;
             }
             else {
-                tb.Text = text;
+                TextViewer.Text = text;
             }
-
-            this.FindControl<TextBlock>("TitleBox")!.Text = title;
-            this.FindControl<Button>("Copy")!.Click += async (_, _) => {
-                if (formatting == Formatting.Markdown) {
-                    await Application.Current!.Clipboard!.SetTextAsync($"**{title}**\n\n{text}");
-                    return;
-                }
-
-                await Application.Current!.Clipboard!.SetTextAsync($"**{title}**\n```\n{text}\n```");
-            };
         }
 
-        public static void ShowSync(string text, string title = "Notice", MessageBoxButtons buttons = MessageBoxButtons.Ok, Formatting formatting = Formatting.None, MaterialIconKind? icon = null)
+        public static async Task<DialogResult> ShowDialog(string text, string title = "Notice", DialogButtons dialogButtons = DialogButtons.Ok, Formatting formatting = Formatting.None, WindowOptions? options = null)
         {
-            using var source = new CancellationTokenSource();
-            Show(text, title, buttons, formatting, icon).ContinueWith(t => source.Cancel(), TaskScheduler.FromCurrentSynchronizationContext());
-            Dispatcher.UIThread.MainLoop(source.Token);
-        }
+            options ??= WindowOptions.Dialog;
+            MessageBox dialog = new(text, formatting);
+            GenericWindow window = new WindowBuilder()
+                .WithContent(dialog)
+                .WithTitle(title)
+                .WithWindowOptions(options)
+                .WithMaxBounds(options.CanResize ? double.NaN : 400, options.CanResize ? double.NaN : 300)
+                .WithMinBounds(200, 120)
+                .WithDialogButtons(dialogButtons)
+                .WithChromeBar(dialog)
+                .Build();
 
-        public static async Task<DialogResult> Show(string text, string title = "Notice", MessageBoxButtons buttons = MessageBoxButtons.Ok, Formatting formatting = Formatting.None, MaterialIconKind? icon = null)
-        {
-            MessageBox msgbox = new(title, text, formatting, icon);
-            var res = DialogResult.Ok;
+            await window.ShowDialog(App.View);
 
-            var buttonPanel = msgbox.FindControl<StackPanel>("Buttons")!;
-            var close = msgbox.FindControl<Button>("Close")!;
-
-            close.Click += (_, __) => {
-                res = DialogResult.Cancel;
-                msgbox.Close();
-            };
-
-            void AddBtn(string caption, DialogResult r, bool def = false, int mode = 0)
-            {
-                var btn = new Button {
-                    Content = caption,
-                    HorizontalContentAlignment = HorizontalAlignment.Center,
-                    Margin = new Thickness(10, 0, 0, 0),
-                    MinWidth = 67,
-                    IsDefault = mode == 1,
-                    IsCancel = mode == 2
-                };
-
-                btn.Click += (_, __) => {
-                    res = r;
-                    msgbox.Close();
-                };
-
-                buttonPanel.Children.Add(btn);
-                if (def) res = r;
-            }
-
-            if (buttons == MessageBoxButtons.Ok || buttons == MessageBoxButtons.OkCancel)
-                AddBtn("Ok", DialogResult.Ok, true, 1);
-
-            if (buttons == MessageBoxButtons.YesNo || buttons == MessageBoxButtons.YesNoCancel) {
-                AddBtn("Yes", DialogResult.Yes, mode: 1);
-                AddBtn("No", DialogResult.No, true);
-            }
-
-            if (buttons == MessageBoxButtons.OkCancel || buttons == MessageBoxButtons.YesNoCancel)
-                AddBtn("Cancel", DialogResult.Cancel, true, 2);
-
-
-            var tcs = new TaskCompletionSource<DialogResult>();
-            msgbox.Closed += delegate { tcs.TrySetResult(res); };
-
-            if (App.View != null) {
-                await msgbox.ShowDialog(App.View);
-            }
-            else {
-                msgbox.Show();
-            }
-
-            return await tcs.Task;
+            return window.Result;
         }
     }
 }
